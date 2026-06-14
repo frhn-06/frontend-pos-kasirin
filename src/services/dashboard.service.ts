5c3439bd8b40dd922e03947f077b890f1db8b876
frhn-06 <farhanmunif233@gmail.com>
2026-06-15 01:02:23 +0700

    fix: perubahan url orderservice

--- src/services/dashboard.service.ts
+++ src/services/dashboard.service.ts
@@ -2,13 +2,13 @@ import instance from "@/libs/axios";
 import endpoint from "./endpoint";
 
 const dashboardService = {
-    summary: () => instance.get(`${endpoint.DASHBOARD}/summary`),
+    summary: () => instance.get(`${endpoint.DASHBOARD}/owner-summary`),
 
     trendSales: () => instance.get(`${endpoint.DASHBOARD}/sales-trend`),
 
-    topProducts: () => instance.get(`${endpoint.DASHBOARD}/top-products`),
+    topProducts: () => instance.get(`${endpoint.DASHBOARD}/owner-top-products`),
 
-    lastOrders: () => instance.get(`${endpoint.DASHBOARD}/last-orders`)
+    lastOrders: () => instance.get(`${endpoint.DASHBOARD}/owner-last-orders`)
 }
 
 export default dashboardService;
