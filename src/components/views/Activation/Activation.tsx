d8ac8c954e0c2c22e420402606d3556c684bf07c
frhn-06 <farhanmunif233@gmail.com>
2026-06-29 12:34:50 +0700

    fix: activation polsing

--- src/components/views/Activation/Activation.tsx
+++ src/components/views/Activation/Activation.tsx
@@ -1,3 +1,4 @@
+import { Button, Link } from '@heroui/react';
 import Image from 'next/image';
 
 const Activation = (props: {status: "success" | "failed"}) => {
@@ -6,23 +7,31 @@ const Activation = (props: {status: "success" | "failed"}) => {
     } = props;
 
     return (
-        <div className='w-64'>
-          {status === "success" ? (
-            <>
-              <Image src="/auth/success.png" alt='failed' width={1080} height={1080} className='w-full' />
-              <h1 className='text-blue-500 text-center text-xl mt-6 font-semibold'>
-                Aktivasi akun berhasil
-              </h1>
-            </>
-          ) : (
-            <>
-              <Image src="/auth/failed.png" alt='failed' width={1080} height={1080} className='w-full' />
-              <h1 className='text-blue-500 text-center text-xl mt-6 font-semibold'>
-                Aktivasi akun gagal
-              </h1>
-            </>
+        <>
+          <div className='w-1/2'>
+            {status === "success" ? (
+              <>
+                <Image src="/auth/success.png" alt='failed' width={1080} height={1080} className='w-full' />
+                <h1 className='text-blue-500 text-center text-xl mt-6 font-semibold'>
+                  Aktivasi akun berhasil
+                </h1>
+              </>
+            ) : (
+              <>
+                <Image src="/auth/failed.png" alt='failed' width={1080} height={1080} className='w-full' />
+                <h1 className='text-blue-500 text-center text-xl mt-6 font-semibold'>
+                  Aktivasi akun gagal
+                </h1>
+              </>
+            )}
+          </div>
+        
+          {status === "success" && (
+            <Button as={Link} href='/dashboard' className='bg-yellow-500 text-white fixed bottom-2 right-2'>
+              Dashboard
+            </Button>
           )}
-        </div>
+        </>
     )
 }
 
--- src/pages/auth/register/success/index.tsx
+++ src/pages/auth/register/success/index.tsx
@@ -1,11 +1,22 @@
 import SuccessRegisterLayout from "@/components/layouts/SuccessRegisterLayout/SuccessRegisterLayout";
+import Image from "next/image";
 
 const PageRegisterSuccess = () => {
     return (
         <SuccessRegisterLayout title="register success">
-            <div>
-                sukses registrasi, silahkan ke email untuk aktivasi akun anda
+          <div className="p-4 flex flex-col gap-4 justify-center items-center">
+            <div className="w-1/2">
+              <Image src={`/auth/success.png`} alt="success" width={720} height={720} className="w-full" />
             </div>
+            <div>
+              <h1 className="font-semibold text-lg text-blue-500 text-center">
+                  Success Regitration
+              </h1>
+              <p className="text-blue-500 text-center">
+                  please Check and open your email for activation account 📩
+              </p>
+            </div>
+          </div>
         </SuccessRegisterLayout>
     )
 }
